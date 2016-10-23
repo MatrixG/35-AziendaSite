855f47690f08e9ce5a23ba0d624722fe73f260c4
alexb <alexb@ALEXHOME>
2016-10-23 09:58:28 +0200

    Class Utente first implementation

--- 35 AziendaSite/src/com/alfasoft/bean/Utente.java
+++ 35 AziendaSite/src/com/alfasoft/bean/Utente.java
@@ -2,4 +2,73 @@ package com.alfasoft.bean;
 
 public class Utente {
 
+	protected int id;
+	protected String cognome;
+	protected String nome;
+	protected char ruolo;
+	protected String username;
+	protected String password;
+	
+	
+	public Utente(){
+		
+	}
+	
+	public Utente(String cognome, String nome, char ruolo, String username, String password) {
+		
+		id = 0;
+		this.cognome = cognome;
+		this.nome = nome;
+		this.ruolo = ruolo;
+		this.username = username;
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public char getRuolo() {
+		return ruolo;
+	}
+
+	public void setRuolo(char ruolo) {
+		this.ruolo = ruolo;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
